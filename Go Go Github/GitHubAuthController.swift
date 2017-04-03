47f510ed5a26a1b9be55c0f1ecffd450a81e03b6
Brandon Little <brlittle86@gmail.com>
2017-04-03 15:30:25 -0700

    maybe this will fix my gitignore?

--- Go Go Github/GitHubAuthController.swift
+++ Go Go Github/GitHubAuthController.swift
@@ -13,7 +13,6 @@ class GitHubAuthController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func printTokenPressed(_ sender: Any) {
