ce3ff78076615787d09cd0d93f8d4bc49c87faa8
Brandon Little <brlittle86@gmail.com>
2017-04-03 15:05:02 -0700

    completed lecture code like a boss

---
+++ Go Go Github/Go Go Github/Tokens.swift
@@ -0,0 +1,12 @@
+//
+//  Tokens.swift
+//  Go Go Github
+//
+//  Created by Brandon Little on 4/3/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+import UIKit
+
+let gitHubClientID = "f7d2ed090c3a44c9ff25"
+let gitHubClientSecret = "5ff42ee1fde2cc89b5de8429a8bf0fe12c27a8a8"
